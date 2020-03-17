8d1dea6cc09d056d6a21be96953e5645abf73010
Sasitha Dilshan <sasithadilshan77@gmail.com>
2020-03-17 17:14:20 +0530

    concurrently and morgan

--- api/server.js
+++ api/server.js
@@ -12,11 +12,13 @@ const userModel = require('../api/models/user');
 const validate=require('../api/middleware/validator/register')
 const loginValidate=require('../api/middleware/validator/login')
 const userRoute=require('../api/routes/user')
+const morgan = require('morgan')
 const loginRoute=require('../api/routes/login')
 const adminloginRoute=require('../api/routes/adminLog')
 require('dotenv').config();
 
 
+app.use(morgan('tiny'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
